3caf881b0e8322d20c0dac1c0d1091e59e93db6c
Derrick Chen <derikk@berkeley.edu>
2025-09-09 02:10:38 -0400

    Added random quotes

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, GatewayIntentBits } from 'discord.js';
+import 'dotenv/config';
 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-  ]
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
-client.once("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+// List of quotes
+const quotes = [
+  "Commit sewer slide.",
+  "I need this for my wife.",
+  "FedEx sucks.",
+  "HAH, GAEEEEEE",
+  "An issue of one's skill.",
+  "Y'all have no idea how many quotes I put in this.",
+  "There's always a new one..."
+];
 
-client.on("messageCreate", (message) => {
+client.on('messageCreate', (message) => {
+  // Ignore messages from bots
   if (message.author.bot) return;
 
-  // If the message has an attachment (like an image)
+  // Check if the message has an image attachment
   if (message.attachments.size > 0) {
-    message.channel.send("I need this for my wife.");
+    // Pick a random quote
+    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    message.channel.send(randomQuote);
   }
 });
 
 client.login(process.env.BOT_TOKEN);
+
